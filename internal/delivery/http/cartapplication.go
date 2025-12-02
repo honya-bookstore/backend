5f34559043ec2e6452ecfee88fbdeb531970a56e
NTGNguyen <ntgnguyen@duck.com>
2025-12-02 13:11:04 +0700

    chore: remove unhandled function in application

--- internal/delivery/http/cartapplication.go
+++ internal/delivery/http/cartapplication.go
@@ -5,7 +5,6 @@ import "context"
 type CartApplication interface {
 	Get(ctx context.Context, param GetCartRequestDTO) (*CartResponseDTO, error)
 	GetByUser(ctx context.Context, param GetCartByUserRequestDTO) (*CartResponseDTO, error)
-	GetMine(ctx context.Context, param GetCartByUserRequestDTO) (*CartResponseDTO, error)
 	Create(ctx context.Context, param CreateCartRequestDTO) (*CartResponseDTO, error)
 	Update(ctx context.Context, param UpdateCartItemRequestDTO) (*CartResponseDTO, error)
 	AddItem(ctx context.Context, param CreateCartItemRequestDTO) (*CartResponseDTO, error)
