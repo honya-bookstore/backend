a6d1a371e38a76cce1776056dfb694d1f82316f9
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-12-11 11:14:43 +0700

    fix(paradedb-index): missing publisher index

--- database/paradedb-index.sql
+++ database/paradedb-index.sql
@@ -3,7 +3,7 @@ USING bm25 (id, name, description)
 WITH (key_field = 'id');
 
 CREATE INDEX IF NOT EXISTS books_search_idx ON books
-USING bm25 (id, title, author, description)
+USING bm25 (id, title, author, description, publisher)
 WITH (key_field = 'id');
 
 CREATE INDEX IF NOT EXISTS medium_search_idx ON medium
